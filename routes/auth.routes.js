b566cd29f8860f5fc2511ab6f5ed5b7b0ca7f81a
MrAFerreira <andre.f.v.ferreira@gmail.com>
2022-03-12 14:34:29 +0000

    dev commit

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,10 +8,7 @@ const jwt = require('jsonwebtoken');
 // How many rounds should bcrypt run the salt (default [10 - 12 rounds])
 const saltRounds = 10;
 
-// Require the User model in order to interact with the database
 const User = require('../models/User.model');
-
-// Require necessary (isLoggedOut and isLiggedIn) middleware in order to control access to specific routes
 const { isAuthenticated } = require('../middleware/jwt.middleware');
 
 router.get('/verify', isAuthenticated, (req, res, next) => {
